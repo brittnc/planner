534fecb8724296faa9c531a6870796b945fe0807
brittnc <84823393+brittnc@users.noreply.github.com>
2021-09-02 23:57:29 -0400

    started structure of code

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr("id");
+
+        console.log(value, time);
+        localStorage.setItem(time, value);
+    });
+});
