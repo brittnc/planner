c4f617fd27d0712e97fb0495ec865af84f803df2
brittnc <84823393+brittnc@users.noreply.github.com>
2021-09-07 00:36:14 -0400

    modified html, removed js

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".saveBtn").on("click", function() {
-        var value = $(this).siblings('.description').val();
-        var time = $(this).parent().attr("id");
-
-        console.log(value, time);
-        localStorage.setItem(time, value);
-    });
-});
